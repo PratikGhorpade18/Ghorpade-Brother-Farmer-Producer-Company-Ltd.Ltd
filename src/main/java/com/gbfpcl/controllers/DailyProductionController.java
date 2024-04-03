170812919b418fd0038f3ade828ed0231bd3dfe4
Pratik Ghorpade <Pratik Ghorpade@RNT-PUN-1201.ho.rnt.ai>
2024-04-03 14:39:05 +0530

    added cors

--- src/main/java/com/gbfpcl/controllers/DailyProductionController.java
+++ src/main/java/com/gbfpcl/controllers/DailyProductionController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import com.gbfpcl.service.ExportDetailsService;
 
 @RestController
 @RequestMapping("/Ghorpade-Brother/DailyReport")
+@CrossOrigin
 public class DailyProductionController {
 	
 	@Autowired
